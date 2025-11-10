ed0d02ce6fbbffdfa1d9d2507e169c6f9f530f9a
Elisa_Sánchez_Martínez <elisasanchezmartinez11@gmail.com>
2025-11-10 00:04:33 -0600

    Initial commit: Estructura de carpetas

---
+++ src/main/java/com/horarios/horarios_unsis/HorariosUnsisApplication.java
@@ -0,0 +1,13 @@
+package com.horarios.horarios_unsis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorariosUnsisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorariosUnsisApplication.class, args);
+	}
+
+}
